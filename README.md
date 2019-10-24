6e42f041b80a3a57652d80463f65104a08d767c5
Brian Birtles <birtles@gmail.com>
2019-10-24 18:57:46 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,27 @@
+A simplified version of deep-equal intended for comparing objects that are being
+serialized to/from JSON:
+
+a)  Treats undefined as null as different
+    (since they produce different JSON output)
+
+b)  Treats a missing property and an undefined property as equal
+    (since they produce the same JSON output)
+
+c)  Only deals with POD for now since that's typically what you serialize and
+    trying to deal other types like deep-equal does can lead to lots of unwanted
+    dependencies.
+
+    e.g. deep-equal has the following dependency chain:
+
+        deep-equal -> is-regex -> has -> function-bind
+
+    and function-bind does `eval()` (actually `Function(<string>)`) which causes
+    problems for CSP.
+
+Furthermore, this library is mostly tailored towards using browser APIs that are
+commonly available. It uses Object.is which IE doesn't support but none of our
+projects care about supporting IE.
+
+In fact, if you're reading this, you probably shouldn't use it. It's basically
+just trying to compare the result of `JSON.stringify()` on two objects but
+allowing the order of keys to differ.
---
+++ src/index.test.ts
@@ -0,0 +1,64 @@
+import { jsonEqualish } from './index';
+
+describe('jsonEqualish', () => {
+  it('should compare various things as the same', () => {
+    const pairs = [
+      [undefined, undefined],
+      [null, null],
+      ['', ''],
+      ['a', 'a'],
+      [0, 0],
+      [-0, +0],
+      [NaN, NaN],
+      [Infinity, Infinity],
+      [-Infinity, -Infinity],
+      [7, 7],
+      [{}, {}],
+      [{}, { a: undefined }], // <-- This is an important test!
+      [{ a: 'abc', b: undefined }, { a: 'abc' }],
+      [{ a: 1 }, { a: 1 }],
+      [[1], [1]],
+      [[1, 2], [1, 2]],
+      [[], []],
+      [{ a: [7, 8] }, { a: [7, 8] }],
+      [{ a: { b: 2 } }, { a: { b: 2 } }],
+    ];
+
+    for (const [a, b] of pairs) {
+      // Sanity check our understanding of JSON
+      expect(JSON.stringify(a)).toEqual(JSON.stringify(b));
+      expect(jsonEqualish(a, b)).toBe(true);
+    }
+  });
+
+  it('should compare objects as the same even when their object key order differs', () => {
+    const pairs = [[{ a: 1, b: 2 }, { b: 2, a: 1 }]];
+
+    for (const [a, b] of pairs) {
+      // Sanity check our understanding of JSON
+      expect(JSON.stringify(a)).not.toEqual(JSON.stringify(b));
+      expect(jsonEqualish(a, b)).toBe(true);
+    }
+  });
+
+  it('should compare various things as different', () => {
+    const pairs = [
+      [{ a: null }, { a: undefined }],
+      [{ a: 1 }, { a: 2 }],
+      [{ a: 1, b: 2 }, { b: 1, a: 2 }],
+      [{ a: 1, b: 2 }, { a: 2, b: 1 }],
+      [[1], [2]],
+      [[1], [1, 2]],
+      [[1, 2], [1]],
+      [['a'], [1]],
+      [[1, 2], [2, 1]],
+      [{ a: { b: 2 } }, { a: { b: 3 } }],
+    ];
+
+    for (const [a, b] of pairs) {
+      // Sanity check our understanding of JSON
+      expect(JSON.stringify(a)).not.toEqual(JSON.stringify(b));
+      expect(jsonEqualish(a, b)).toBe(false);
+    }
+  });
+});
